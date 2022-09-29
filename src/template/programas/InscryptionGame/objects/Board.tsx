3fb1e8e6129c200890ec40cbac4e7e5f86b96e9c
Leandro Cruz <leandro1803@outlook.com>
2022-09-29 07:54:27 +0100

    sla

--- src/template/programas/InscryptionGame/objects/Board.tsx
+++ src/template/programas/InscryptionGame/objects/Board.tsx
@@ -1,6 +1,25 @@
-import { useRef, useState } from "react"
-import {Card} from "./Card"
+import { MutableRefObject, useRef, useState } from "react"
+import "../styles/Crad.css"
 
+type CardProps = {
+    type?: string,
+    price?:number,
+    refA?:MutableRefObject<(HTMLDivElement | null)[]>,
+    onMouseDown?:Function
+}
+
+
+export function Card({type="", price=0, refA, onMouseDown=()=>{}}:CardProps){
+
+    return (
+        <div style={{paddingBottom:"100px"}}>
+            <div ref={refA!=undefined?(ref)=>{refA.current.push(ref)}:(ref)=>ref}  className="Card" onMouseDown={onMouseDown()}>
+                <img data-key={price} src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.caymancompass.com%2Fwp-content%2Fuploads%2F2016%2F10%2FCTF-Queen-Butterfly4.jpg" alt="" />
+            </div>
+        </div>
+    )
+    
+}
 export function Board(){
     
     const myRef = useRef<(HTMLDivElement)[]>([])
